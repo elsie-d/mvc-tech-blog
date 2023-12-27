e49e05eeacc74f5795d6f70bc26ccdf8a38ef6e2
unknown <elsie.dh@gmail.com>
2023-12-26 17:00:48 -0800

    seeds and associations

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,57 @@
 const router = require('express').Router();
-//const { User } = require('../models');
+const { User , Blog } = require('../models');
 //const withAuth = require('../utils/auth');
 
+
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all blogs and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      blogs
+      //logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+/* 
+// Initial test
 router.get('/', (req, res) => {
   const message = 'success'
   res.render('homepage', {
     message
   })
-});
+}); */
 
 router.get('/dashboard', (req,res) => {
   const message2 = 'dash success'
   res.render('homepage', {
     message2
+  })
+})
+
+router.get('/blog', (req,res) => {
+  const message3= 'blog success'
+  res.render('homepage', {
+    message3
   })
 })
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,24 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Blog } = require('../models');
+
+const userData = require('./userData.json');
+const blogsData = require('./blogsData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  await sequelize.sync({ force: true });
-
+  console.log(User);
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+  console.log(users)
 
- /** 
-  * DO SEEDS HERE
-  */
-
+  for (const blog of blogsData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
   process.exit(0);
 };
 
